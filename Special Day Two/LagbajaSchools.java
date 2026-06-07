09bf1166584b3347945edb322ddefc5a66f8664b
QueenColly <colleteumeh0@gmail.com>
2026-06-07 23:35:52 +0100

    first commit

--- Special Day Two/LagbajaSchools.java
+++ Special Day Two/LagbajaSchools.java
@@ -16,53 +16,83 @@ public static void main(String[] args){
      System.out.println("\nYou've entered " + studentNumber + " as the total number of students and " + subjects + " of the total number of subjects they offer per term.  ");
     
 
-    System.out.println("\nSaving >>>>>>>>>>>>>>>");
-    System.out.println("\nSaved Successfully");
+    System.out.println("""
+
+    Saving >>>>>>>>>>>>>>>
+    Saved Successfully!""");
+    System.out.println();
 
     int [][] grade = new int [studentNumber][subjects];
 
             int counter = 1;
-        for(int index = 0; index < grade.length; index++){
-
-            for(int count =0; count < grade[index].length ; count++){
-
-    
-
-    System.out.print("\nPlease enter the student's score:  ");
-    int score = input.nextInt();
-    grade[index][count] = score;
-
-    
+            double []total = new double [studentNumber];
+            double [] average = new double [studentNumber];
            
-       
-}
-}
+            int count ;
+            int index;
+        for( index = 0; index < grade.length; index++){
+            for( count =0; count < grade[index].length ; count++){
+            System.out.println("\nEntering score for student "+ (index + 1) + ": ");
+          
+            System.out.print("Enter score for subject "+ (count + 1) + ": ");
+                            
+            int score = input.nextInt();
+            grade[index][count] = score;
+            
+            System.out.print("""
+            Saving >>>>>>>>>>>>>>>
+            Saved Successfully!""");
+            System.out.println();                     
+                }
 
+        }
+//
     System.out.println("\nThe grades are:  "+ Arrays.deepToString(grade));
 
-
-             double total =0;
-        for(int index = 0; index < grade.length; index++){
-    
-            for(int count = 0; count < grade[index].length; count++){
-
-        total += grade[index][count];
+         for(  index = 0; index < grade.length; index++){
+            for( count = 0; count < grade[index].length; count++){
+                    total[index] += grade[index][count];
 }
+
        
-
-        double average = total/grade[index].length;
-        
-         System.out.println("\nTotal score of student "+ counter + " is: " + total);
-                counter++;
-        System.out.println("\nThe average is: " + average);
-}
-   
-
+     average[index] = total[index]/subjects;
     
 
-
 }
+    
+       
+     System.out.println("\nTotal score of student  is: " + Arrays.toString(  total));
+     System.out.println("The average is: " +    Arrays.toString( average));
+
+                 System.out.println("=====================================================================================================");
+       System.out.printf("%20s","STUDENT");
+            for(int outer = 1; outer <= subjects; outer++){
+                System.out.printf("%10s", "SUB"+ outer);
+            }
+
+         System.out.printf("%13s%13s%13s%n",  "TOTAL", "AVERAGE","POSITION");
+                 System.out.println("=====================================================================================================");
+  
+    
+
+    for( int indexx = 0; indexx < studentNumber ; indexx++){
+       System.out.printf("%20s%d", "Student " , (indexx+1));
+
+          
+          for(int counts = 0; counts < subjects;counts++){
+                System.out.printf("%10d", grade[indexx][counts]);
+
+                
+            }
+               
+                System.out.printf("%10.0f%10.2f%n",total[indexx], average[indexx]);
+        }   
+    
+                System.out.println("=====================================================================================================");
 
 
 
-}
+
+            
+        }
+    }
